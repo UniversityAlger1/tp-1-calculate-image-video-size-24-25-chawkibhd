ba5fdaae5a7021c94b06106ab4743e8562f451fe
Belhaddad Chawki <chakibhdbhd@icloud.com>
2024-11-22 01:33:03 +0100

    remplissage du code

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -6,9 +6,33 @@
 //   u: Unit of the output value. It could be 'bt' bits, 'ko' kilobits, 'mo' megabits, 'go' gigabits
 // Return value
 //   colored image size Bitmap (based on the unit passed parametter)
-float coloredBitmap(int w, int h, char* unit) {
-   // YOUR CODE HERE - BEGIN
+float coloredBitmap(int w, int h, char *unit)
+{
+    int num_pixels = w * h;
+    int bits_per_pixel = 24; 
+    float total_bits = num_pixels * bits_per_pixel;
 
-   // YOUR CODE HERE - END
-   return 0;
+    if (strcmp(unit, "bt") == 0)
+    {
+        return total_bits;
+    }
+    else if (strcmp(unit, "ko") == 0)
+    {
+        return (total_bits / 8) / 1024;
+    }
+    else if (strcmp(unit, "mo") == 0)
+    {
+        return (total_bits / 8) / (1024 * 1024);
+    }
+    else if (strcmp(unit, "go") == 0)
+    {
+        return (total_bits / 8) / (1024 * 1024 * 1024);
+    }
+    else
+    {
+        // Invalid unit
+        return 0;
+    }
+    // YOUR CODE HERE - END
+    return 0;
 }
--- video.c
+++ video.c
@@ -8,9 +8,33 @@
 //   unit: Unit of the output value. It could be 'bt' byte, 'ko' kilobits, 'mo' megabits, 'go' gigabits
 // Return value
 //   colored video size (based on the unit passed parametter)
-float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
-   // YOUR CODE HERE - BEGIN
+float video(int w, int h, int durationMovie, int durationCredits, int fps, char *unit)
+{
+   float bitsPerColorPixel = 24;
+   float videoSizeColor = w * h * bitsPerColorPixel * durationMovie * fps;
+   float videoSizeBW = w * h * durationCredits * fps;
+   float totalSizeBits = videoSizeColor + videoSizeBW;
+   float finalSize;
 
-   // YOUR CODE HERE - END
-   return 0;
+   if (strcmp(unit, "bt") == 0)
+   {
+      finalSize = totalSizeBits;
+   }
+   else if (strcmp(unit, "ko") == 0)
+   {
+      finalSize = totalSizeBits / 1024;
+   }
+   else if (strcmp(unit, "mo") == 0)
+   {
+      finalSize = totalSizeBits / (1024 * 1024);
+   }
+   else if (strcmp(unit, "go") == 0)
+   {
+      finalSize = totalSizeBits / (1024 * 1024 * 1024);
+   }
+   else
+   {
+      return -1.0f;
+   }
+   return finalSize / 8;
 }
